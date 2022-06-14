9f27dd5907205eb0f5763ccfdc3d7a1daacceb82
JaeHun-PC <zakbuill510@gmail.com>
2022-06-14 23:42:41 +0900

    modify home, design

---
+++ app/src/main/java/com/oldeee/user/ui/orderlog/detail/OrderLogDetailFragment.kt
@@ -0,0 +1,30 @@
+package com.oldeee.user.ui.orderlog.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
+import com.oldeee.user.R
+import com.oldeee.user.base.BaseFragment
+import com.oldeee.user.databinding.FragmentOrderLogDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OrderLogDetailFragment : BaseFragment<FragmentOrderLogDetailBinding, OrderLogDetailViewModel, NavArgs>() {
+    override val layoutId: Int = R.layout.fragment_order_log_detail
+    override val viewModel: OrderLogDetailViewModel by viewModels()
+    override val navArgs: NavArgs by navArgs()
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+    override fun initDataBinding() {
+
+    }
+
+}
