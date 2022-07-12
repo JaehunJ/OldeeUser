91bcb6085a00481b91b12592c338e81e736670e3
jaehun <510jaehun@bigcare.co.kr>
2022-07-12 14:21:06 +0900

    faq 작업

--- app/src/main/java/com/oldee/user/ui/faq/FaqFragment.kt
+++ app/src/main/java/com/oldee/user/ui/faq/FaqFragment.kt
@@ -3,6 +3,8 @@ package com.oldee.user.ui.faq
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,12 +28,42 @@ class FaqFragment : BaseFragment<FragmentFaqBinding, FaqViewModel, NavArgs>() {
             binding.layoutFaqItem.cbArr.isChecked = !binding.layoutFaqItem.cbArr.isChecked
         }
         binding.layoutFaqItem.cbArr.setOnCheckedChangeListener { compoundButton, b ->
-            binding.layoutFaqItem.tvContents.visibility = if(b) View.VISIBLE else View.GONE
+            val card = binding.layoutFaqItem
+            if(b){
+                TransitionManager.beginDelayedTransition(
+                    card.cvRoot,
+                    AutoTransition()
+                )
+                card.clHide.visibility = View.VISIBLE
+            }else{
+                TransitionManager.beginDelayedTransition(
+                    card.cvRoot,
+                    AutoTransition()
+                )
+                card.clHide.visibility = View.GONE
+            }
+//            binding.layoutFaqItem.tvContents.visibility = if(b) View.VISIBLE else View.GONE
         }
 
         binding.clKakao.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://pf.kakao.com/_QuRxmb")))
         }
+//        val card = binding.layoutFaqItem
+//        card.clFixed.setOnClickListener {
+//            if(card.clHide.visibility == View.VISIBLE){
+//                TransitionManager.beginDelayedTransition(
+//                    card.cvRoot,
+//                    AutoTransition()
+//                )
+//                card.clHide.visibility = View.GONE
+//            }else{
+//                TransitionManager.beginDelayedTransition(
+//                    card.cvRoot,
+//                    AutoTransition()
+//                )
+//                card.clHide.visibility = View.VISIBLE
+//            }
+//        }
     }
 
     override fun initDataBinding() {
